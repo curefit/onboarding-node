9f4847fe6ec455a568845a53a8098eb1cf1ea80b
Pushpendra <pushpendra.singh@curefit.com>
2021-07-12 00:50:25 +0530

    Add datatypes and function sample code

---
+++ basics/datatypes.js
@@ -0,0 +1,9 @@
+let color = "Blue"
+console.log("Color: ",color)
+
+let age = 30
+console.log("Age: ", age)
+
+// now assign number to color variable
+color = 5
+console.log("Color variable is now number: ", color)
---
+++ basics/testfunctions.js
@@ -0,0 +1,6 @@
+function concat(str1, str2) {
+	return str1 + " " + str2
+}
+
+const cctd = concat("Super", "Fit")
+console.log(cctd)
