ce34c3fc62218bfbc268d600bd76d99173b38510
pravesh-curefit <pravesh@curefit.com>
2021-07-13 19:42:57 +0530

    Add fetch

--- src/service/ProductService.ts
+++ src/service/ProductService.ts
@@ -3,6 +3,7 @@ import {BASE_TYPES, ILogger} from "@curefit/base";
 import {FetchUtilV2} from "@curefit/base";
 import {Product} from "../common/Product";
 import {IProductService} from "./IProductService";
+import fetch from "node-fetch"
 
 @injectable()
 export class ProductService implements IProductService {
@@ -17,15 +18,15 @@ export class ProductService implements IProductService {
     }
 
     public async getProduct(productId: string): Promise<Product> {
-        // const response = await fetch(ProductService.getProductUrl(productId), this.fetchUtil.get())
-        // return this.fetchUtil.parseResponse<Product>(response)
-        return {
-            id: productId,
-            name: "Product title",
-            description: "Description",
-            price: 100,
-            type: "Shirt",
-            imageUrl: "url"
-        }
+        const response = await fetch(ProductService.getProductUrl(productId), this.fetchUtil.get())
+        return this.fetchUtil.parseResponse<Product>(response)
+        // return {
+        //     id: productId,
+        //     name: "Product title",
+        //     description: "Description",
+        //     price: 100,
+        //     type: "Shirt",
+        //     imageUrl: "url"
+        // }
     }
 }
