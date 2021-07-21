5ff451b8ddb30f9e38e94dee643de2756710a703
bharath-12345 <vedartham.bharath@curefit.com>
2021-07-21 10:55:30 +0530

    Add rollbar

--- src/controller/EchoController.ts
+++ src/controller/EchoController.ts
@@ -4,14 +4,22 @@ import {inject} from "inversify";
 import {TYPES} from "../ioc/types";
 import {IOrderService} from "../service/IOrderService";
 import {CreateOrderParams} from "../common/CreateOrderParams";
+import {ERROR_COMMON_TYPES, RollbarService} from "@curefit/error-common";
 
 @controller("/echo")
 export class EchoController {
     constructor(
+        @inject(ERROR_COMMON_TYPES.RollbarService) private rollbarService: RollbarService,
     ) {}
 
     @httpGet("/")
     public echoStuff(request: express.Request): Promise<string> {
+        let i = 0;
+        while (i < 10) {
+            this.rollbarService.sendError(new Error("Shit's going down!!"));
+            i += 1;
+        }
+
         return Promise.resolve("Hello World");
     }
 }
